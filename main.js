a1fa2edf0b0d6607d6cf2dd2b5b5cc9c80786409
Shagj Batjargal <shagj444@gmail.com>
2024-09-24 13:49:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
     plane.rotation.x = Math.PI / 2;
     plane.rotation.y = 0;
     plane.scale.multiplyScalar(0);
-    const anchor = mindarThree.addAnchor(0);
+    const anchor = mindarThree.addAnchor(1);
     anchor.group.add(plane);
 
     anchor.onTargetFound = () => {
